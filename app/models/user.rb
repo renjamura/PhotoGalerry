20c96a694324975352ba511de1990cbbeb685468
Murat Djumaliev <renja.dizz@gmail.com>
2017-10-28 12:51:17 +0600

    Add reference to Photo
    User has_many :photos

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  has_many :photos
+ 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-	validates :name, presence: true, length: {maximum: 50}
+
+  validates :name, presence: true, length: {maximum: 50}
+  
 end
