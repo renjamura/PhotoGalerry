0d90bb827337a23497c247b70fb829669f4ca103
Murat Djumaliev <renja.dizz@gmail.com>
2017-10-28 18:17:01 +0600

    Change admin panel

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,15 +1,32 @@
 ActiveAdmin.register User do
-# See permitted parameters documentation:
-# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-#
-# permit_params :list, :of, :attributes, :on, :model
-#
-# or
-#
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if params[:action] == 'create' && current_user.admin?
-#   permitted
-# end
+
+permit_params :name, :email, :password, :admin
+
+index do
+  selectable_column
+  id_column
+  column :name
+  column :email
+  column :admin
+  actions
+end
+
+show do
+  attributes_table do
+    row :name
+    row :email
+    row :admin
+  end
+end
+
+form do |f|
+  f.inputs do
+    f.input :name
+    f.input :email
+    f.input :password
+    f.input :admin
+  end
+  f.actions
+end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,9 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :photos do
-  	resources :comments
+  	resources :comments, only: [:create]
   end
-  resources :users
+  resources :users, only: [:show]
   root 'photos#index'
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
