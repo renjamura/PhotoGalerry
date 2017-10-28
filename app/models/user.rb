c0785a154782ca2eecdb5d3ceab2db0c92f853df
Murat Djumaliev <renja.dizz@gmail.com>
2017-10-28 17:02:42 +0600

    Add dependent destroy

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  has_many :photos
-  has_many :comments
+  has_many :photos, :dependent => :destroy
+  has_many :comments,:dependent => :destroy
  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
