8efd2baaea8704d7ccc1b385edca0cfe70beaedf
Murat Djumaliev <renja.dizz@gmail.com>
2017-10-28 14:57:33 +0600

    Add score attribute to comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171028080632) do
+ActiveRecord::Schema.define(version: 20171028084300) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20171028080632) do
     t.integer "photo_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "score"
     t.index ["photo_id"], name: "index_comments_on_photo_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
