09b4bd4fe804b4a6f2d2faa869d544833c53e49d
Murat Djumaliev <renja.dizz@gmail.com>
2017-10-28 16:13:29 +0600

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,52 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+  admin_user = User.create(name: 'Admin', email: 'admin@example.com', password: 'adminadmin', password_confirmation: 'adminadmin', admin: true)
+  user_first = User.create(name: 'Murat', email: 'murat@example.com', password: '12345678', password_confirmation: '12345678')
+  user_second = User.create(name: 'John', email: 'john@example.com', password: '12345678', password_confirmation: '12345678')
+  user_third = User.create(name: 'Mike', email: 'mike@example.com', password: '12345678', password_confirmation: '12345678')
+  user_forth = User.create(name: 'Alice', email: 'alice@example.com', password: '12345678', password_confirmation: '12345678')
+  user_fifth = User.create(name: 'Miley', email: 'miley@example.com', password: '12345678', password_confirmation: '12345678')
+
+  photo_first = Photo.create(name: 'Desert', user: user_first)
+  photo_first.image = Rails.root.join("app/assets/images/fixtures/Desert.jpg").open
+  photo_first.save!
+  photo_second = Photo.create(name: 'Coala', user: user_first)
+  photo_second.image = Rails.root.join("app/assets/images/fixtures/coala.jpg").open
+  photo_second.save!
+  photo_third = Photo.create(name: 'Chrysanthemum', user: user_first)
+  photo_third.image = Rails.root.join("app/assets/images/fixtures/Chrysanthemum.jpg").open
+  photo_third.save!
+
+  photo_forth = Photo.create(name: 'Hydrangeas', user: user_second)
+  photo_forth.image = Rails.root.join("app/assets/images/fixtures/Hydrangeas.jpg").open
+  photo_forth.save!
+  photo_fifth = Photo.create(name: 'Jellyfish', user: user_second)
+  photo_fifth.image = Rails.root.join("app/assets/images/fixtures/Jellyfish.jpg").open
+  photo_fifth.save!
+
+  photo_six = Photo.create(name: 'Lighthouse', user: user_third)
+  photo_six.image = Rails.root.join("app/assets/images/fixtures/Lighthouse.jpg").open
+  photo_six.save!
+
+  photo_seven = Photo.create(name: 'Penguins', user: user_forth)
+  photo_seven.image = Rails.root.join("app/assets/images/fixtures/Penguins.jpg").open
+  photo_seven.save!
+
+  photo_eigth = Photo.create(name: 'Tulips', user: user_fifth)
+  photo_eigth.image = Rails.root.join("app/assets/images/fixtures/Tulips.jpg").open
+  photo_eigth.save!
+
+  photo_admin = Photo.create(name: 'Admin', user: admin_user)
+  photo_admin.image = Rails.root.join("app/assets/images/fixtures/admin.jpg").open
+  photo_admin.save!
+  
+  users_array = [user_first, user_second, user_third, user_forth, user_fifth]
+  5.times  do
+    photo_first_comment = Comment.create(body: 'Good' , user: users_array.sample, photo: photo_first)   	
+  end
+
+  
+
+
+
